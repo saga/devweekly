6a16a63a8bd4a1e26f69bfacf936297912c504f4
saga <sagasw@gmail.com>
2026-07-05 07:57:36 +0800

    addd

--- src/config.ts
+++ src/config.ts
@@ -3,8 +3,8 @@ import type { Site, SocialObjects } from "./types";
 export const SITE: Site = {
   website: "https://devweekly.github.io", // replace this with your deployed domain
   author: "SW",
-  desc: "Developer Weekly 开发者周报，编程技术、架构设计、AI/LLM、产品思维与设计资源。开发周报, 编程周报, 技术周报, programming weekly",
-  title: "开发者周报 Developer Weekly",
+  desc: "Builder Weekly 开发者周报，编程技术、架构设计、AI/LLM、产品思维与设计资源。开发周报, 编程周报, 技术周报, programming weekly",
+  title: "开发者周报 Builder Weekly",
   ogImage: "astropaper-og.jpg",
   lightAndDarkMode: false,
   postPerPage: 5,
